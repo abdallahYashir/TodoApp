91f358ca346fdcf80e7b20f8f5c2fc762082e13b
Abdallah Ramsing <yashircr@gmail.com>
2023-04-02 12:15:49 +0400

    Added the services + boilerplate test

---
+++ src/ToDoService.js
@@ -0,0 +1,57 @@
+import LocalStorageService from "./localStorageService";
+
+class TodoService {
+  constructor() {
+    this.todos = [...this.loadTodos()];
+  }
+
+  addTodo = (text) => {
+    const newTodo = { id: Date.now(), text, completed: false, archived: false };
+    this.todos.push(newTodo);
+    return newTodo;
+  };
+
+  removeTodo = (id) => {
+    const index = this.todos.findIndex((todo) => todo.id === id);
+    if (index !== -1) {
+      const removedTodo = this.todos.splice(index, 1)[0];
+      return removedTodo;
+    }
+  };
+
+  updateTodo = (id, newText) => {
+    const index = this.todos.findIndex((todo) => todo.id === id);
+    if (index !== -1) {
+      this.todos[index].text = newText;
+      return this.todos[index];
+    }
+  };
+
+  completeTodo = (id) => {
+    const index = this.todos.findIndex((todo) => todo.id === id);
+    if (index !== -1) {
+      this.todos[index].completed = true;
+      return this.todos[index];
+    }
+  };
+
+  archivedTodo = (id) => {
+    const index = this.todos.findIndex((todo) => todo.id === id);
+    if (index !== -1) {
+      this.todos[index].archived = true;
+      return this.todos[index];
+    }
+  };
+
+  getAllTodos = () => {
+    return this.todos;
+  };
+
+  loadTodos = () => {
+    this.todos = LocalStorageService.get("todos") || [];
+  };
+
+  save = () => {
+    LocalStorageService.set("todos", this.todos);
+  };
+}
---
+++ src/localStorageService.js
@@ -0,0 +1,10 @@
+const LocalStorageService = {
+  save(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+
+  get(key) {
+    const value = localStorage.getItem(key);
+    return value ? JSON.parse(value) : null;
+  },
+};
