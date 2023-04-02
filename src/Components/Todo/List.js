bf1b319d74264c93cdde81741719f8456a8026db
Abdallah Ramsing <yashircr@gmail.com>
2023-04-02 11:15:28 +0400

    Styling OK

--- src/Components/Todo/List.js
+++ src/Components/Todo/List.js
@@ -1,54 +1,48 @@
 import React from "react";
 import "./List.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function ToDoList() {
   return (
     <div className="todo-list">
       <ul class="list-group">
-        <li class="list-group-item">
-          <input
-            class="form-check-input me-1"
-            type="checkbox"
-            value=""
-            aria-label="..."
-          />
-          First checkbox
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          <div className="content d-flex justify-content-between">
+            <input
+              class="form-check-input me-1"
+              type="checkbox"
+              value=""
+              aria-label="..."
+            />
+            <div className="mx-2">First checkbox</div>
+          </div>
+          <FontAwesomeIcon icon={faTrash} />
         </li>
-        <li class="list-group-item">
-          <input
-            class="form-check-input me-1"
-            type="checkbox"
-            value=""
-            aria-label="..."
-          />
-          Second checkbox
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+          <div className="content d-flex justify-content-between">
+            <input
+              class="form-check-input me-1"
+              type="checkbox"
+              value=""
+              aria-label="..."
+            />
+            <div className="mx-2">Second checkbox</div>
+          </div>
+          <FontAwesomeIcon icon={faTrash} />
         </li>
-        <li class="list-group-item">
-          <input
-            class="form-check-input me-1"
-            type="checkbox"
-            value=""
-            aria-label="..."
-          />
-          Third checkbox
-        </li>
-        <li class="list-group-item">
-          <input
-            class="form-check-input me-1"
-            type="checkbox"
-            value=""
-            aria-label="..."
-          />
-          Fourth checkbox
-        </li>
-        <li class="list-group-item">
-          <input
-            class="form-check-input me-1"
-            type="checkbox"
-            value=""
-            aria-label="..."
-          />
-          Fifth checkbox
+        <li class="list-group-item d-flex justify-content-between align-items-center text-decoration-line-through">
+          <div className="content d-flex justify-content-between">
+            <input
+              class="form-check-input me-1"
+              type="checkbox"
+              value=""
+              aria-label="..."
+              checked="true"
+            />
+            <div className="mx-2 font-italic">Third checkbox</div>
+          </div>
+          <FontAwesomeIcon icon={faTrash} />
         </li>
       </ul>
     </div>
